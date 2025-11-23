18454d8cd0a22de9c72ed043a2a59f72d3d12041
FXL-niu <2397298913@qq.com>
2025-11-24 02:42:26 +0800

    f

--- build.sh
+++ build.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
+set -e
 
 ARGS="$@"
 
-if [ -n "${SVF_DIR}" ] && [ -d "${SVF_DIR}/llvm-16.0.0.obj" ]; then
-  export LLVM_DIR="${SVF_DIR}/llvm-16.0.0.obj"
-  echo "LLVM_DIR: $LLVM_DIR"
-elif [ -n "${LLVM_DIR}" ] && [ -d "${LLVM_DIR}" ]; then
-  echo "LLVM_DIR: $LLVM_DIR"
-else
-  echo "LLVM_DIR not found!"
-  exit 1
+echo "[INFO] Installing LLVM 14..."
+sudo apt-get update
+sudo apt-get install -y llvm-14 llvm-14-dev clang-14 lld-14
+
+echo "[INFO] Switching default LLVM to version 14..."
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-14 100
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-14 100
+sudo update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-14 100
+
+echo "clang version: "
+clang --version
+echo "llvm-config version:"
+llvm-config --version
+
+export LLVM_DIR="/usr/lib/llvm-14/cmake"
+export CMAKE_PREFIX_PATH="/usr/lib/llvm-14/cmake:${CMAKE_PREFIX_PATH}"
+
+# If running inside SVF folder (workflow: first build)
+if [[ "$PWD" == *"/SVF"* ]]; then
+    echo "[INFO] Detected SVF build step."
+    rm -rf build/
+    mkdir build && cd build
+    cmake -DLLVM_DIR=/usr/lib/llvm-14/cmake ..
+    make -j2
+    exit 0
 fi
 
+# Otherwise building student's assignment
+echo "[INFO] Building student's assignment..."
 rm -rf build/
 mkdir build && cd build/
 
@@ -22,3 +42,5 @@ else
 fi
 
 make -j4
+
+echo "[INFO] Build Success!"
